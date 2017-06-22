07453142b253b35cac6f12258a654861c38e1644
JKGzenna <juanlu360@gmail.com>
2017-06-22 22:34:35 +0200

    EjemploFunciones

--- src/ejemplofunciones/EjemploFunciones.java
+++ src/ejemplofunciones/EjemploFunciones.java
@@ -5,11 +5,12 @@ package ejemplofunciones;
  * @author JUAN
  */
 public class EjemploFunciones {
-
+    // declaracion de la funcion, la declaramos static porque va a ser llamada desde la función main
     static int multiplicaPorTres(int p) {
         int r = p * 3;
         return r;
     }
+    // llamada a la función desde la función main
     public static void main(String[] args) {
         int a = 5;
         int b = multiplicaPorTres(a);
